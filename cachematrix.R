44a8d04bb608d925cb55a27f46859154661f701f
ashabe <30386572+ashabe@users.noreply.github.com>
2017-08-20 10:03:22 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## we want to return: a list containing functions that will set and get the matrix,set and get th inverse
+## z is a square invertible matrix    
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(z = matrix()) {
+        
+        
+        
+        invert = NULL
+        set = function(y) {
+                # we use `<<-` to assign a value to an object in an environment 
+                # different from the current environment. 
+                z <<- y
+                invert <<- NULL
+        }
+        get = function() z
+        setinv = function(inverse) invert <<- inverse 
+        getinv = function() invert
+        list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## z is the output of makeCacheMatrix()
+## we want to return the inverse of the original matrix input to makeCacheMatrix()
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+cacheSolve <- function(z, ...) {
+        
+        
+        invert = z$getinv()
+        
+        
+        if (!is.null(invert)){
+                 
+                message("getting cached data")
+                return(invert)
+        }
+        
+        # calculate the inverse 
+        mat.data = z$get()
+        invert = solve(mat.data, ...)
+        
+        # setting the value of the inverse in the cache
+        z$setinv(invert)
+        
+        return(invert)
 }
